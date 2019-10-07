67dd249bffb8d5fe32a58badee00399b40ba1db5
seppe_verhavert <seppe.verhavert@gmail.com>
2019-10-07 16:46:57 +0200

    change probability on race

--- script.js
+++ script.js
@@ -1,17 +1,66 @@
 let btn = document.getElementById('generateBtn');
 btn.addEventListener('click', generateNpc);
 
+let firstLineArray = [
+    "physicalDescription",
+    "age",
+    "gender",
+    "race",
+    "exoticRace",
+    "eyes",
+    "mouth",
+    "hair",
+    "face",
+    "scar",
+    "tattoo",
+    "jewelry",
+    "jewelryMaterial",
+    "clothing",
+    "pets",
+    "occupations",
+];
+let secondLineArray = [
+    "moods",
+    "jobs",
+    "prejudices",
+    "faiths",
+    "quirks",
+    "traits",
+    "factions",
+    "heraldicSigns",
+    "motivations",
+    "sideplots",
+    "fears",
+    "thougths",
+    "titles",
+    "secrets",
+];
+let thirdLineArray = [
+    "hooks",
+    "rumors1",
+    "rumors2",
+    "rumors3",
+];
+
+let firstLine = [];
+let secondLine = [];
+let thirdLine = [];
+
 fetch("https://raw.githubusercontent.com/SeppeVerhavert/3-Line-NPC/master/tables.json")
     .then(response => response.json())
     .then(json => library = json);
 
 function generateNpc() {
-    console.log(library);
+    generateFirstLine();
+    // generateSecondLine();
+    // generateThirdLine();
 }
 
-function rolld(element) {
-    let roll = parseInt(Math.floor(Math.random() * (element) + 1));
-    return roll;
+function generateFirstLine() {
+    console.log(rollTable(library.physicalDescription));
+    console.log(rollTable(library.age));
+    console.log(rollTable(library.gender));
+    console.log(rollTable(library.race));
 }
 
 function rollTable(element) {
