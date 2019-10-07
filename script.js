a048a397b68056ba3353b401f48c58470a157bff
seppe_verhavert <seppe.verhavert@gmail.com>
2019-10-07 16:24:13 +0200

    add json call

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 let btn = document.getElementById('generateBtn');
 btn.addEventListener('click', generateNpc);
 
-
+fetch("https://raw.githubusercontent.com/SeppeVerhavert/3-Line-NPC/master/tables.json")
+    .then(response => response.json())
+    .then(json => library = json);
 
 function generateNpc() {
-    console.log("done");
+    console.log(library);
+}
+
+function rolld(element) {
+    let roll = parseInt(Math.floor(Math.random() * (element) + 1));
+    return roll;
+}
+
+function rollTable(element) {
+    tableroll = Math.floor(Math.random() * element.length);
+    return element[tableroll];
 }
